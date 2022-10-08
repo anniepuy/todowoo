5ddf521e7c1bb74ce46cf182da093ce6edf39297
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2022-10-08 00:56:47 -0500

    initial commit - rebulding in django

---
+++ todo/views.py
@@ -0,0 +1,34 @@
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'todo/home.html')         
+
+def usersignup(request): 
+    if request.method == "GET":
+        return render(request, 'todo/usersignup.html', {'form':UserCreationForm()})
+    else:
+        #Create new user
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/usersignup.html', {'form':UserCreationForm(), 'error': 'Username has already been taken. Please choose a new username.'})
+        else:
+            return render(request, 'todo/usersignup.html', {'form':UserCreationForm(), 'error': 'Passwords did not match.'})
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
