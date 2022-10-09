31939a46b60917bc9770282e7a51e8e17e95601d
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2022-10-08 23:18:04 -0500

    fixed log in request

--- todo/views.py
+++ todo/views.py
@@ -34,8 +34,12 @@ def loginuser(request):
     if request.method == "GET":
         return render(request, 'todo/loginuser.html', {'form':AuthenticationForm()})
     else:
-        user = authenticate(request, username=request.POST['username'] , password=request.POST['password'])
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
             return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error': 'Username and password did not match. Please login.'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
